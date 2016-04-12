2b6a2fd7336531ae3f6b109c140c78f739a43ac6
Petar <milanoslav@yahoo.com>
2016-04-12 14:16:37 +0200

    importanPhonesObject.php kreiran

--- utils/important_phones/models/importantPhonesObject.php
+++ utils/important_phones/models/importantPhonesObject.php
@@ -1,1 +1,37 @@
 <?php
+class importantPhonesObject extends mainObject
+{
+
+    protected $Title;
+    protected $Content;
+    protected $Link;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getTitle(){
+        return $this->Title;
+    }
+
+    public function setTitle($title){
+        $this->Title = $title;
+    }
+
+    public function getContent(){
+        return $this->Content;
+    }
+
+    public function setContent($Content){
+        $this->Content = $Content;
+    }
+
+    public function getLink(){
+        return $this->Link;
+    }
+
+    public function setLink($Link){
+        $this->Link = $Link;
+    }
+}
