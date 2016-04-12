7a00c13596ed85ce7e21c3d21562c259f6304db6
Petar <milanoslav@yahoo.com>
2016-04-12 14:39:34 +0200

    Odradjen 'models' za important phones. (basic)

--- utils/important_phones/models/importantPhonesModel.php
+++ utils/important_phones/models/importantPhonesModel.php
@@ -1,1 +1,64 @@
 <?php
+
+class importantPhonesModel extends page_class
+{
+    protected $table;
+    protected $engine;
+
+
+    public function add(array $data){
+
+    if (!$this->checkData($data)){
+        return false;
+    }
+
+    $importantPhones = new importantPhonesObject();
+    $importantPhones ->setTitle($data["title"]);
+    $importantPhones ->setContent($data["content"]);
+    $importantPhones ->setLink($data["link"]);
+
+    $this->_add($importantPhones);
+}
+
+    private function checkData(array $data){
+    return true;
+}
+
+    protected function _add(importantPhonesObject $importantPhones){
+
+}
+
+    public function update(array $data)
+{
+    if (!$this->checkData($data)){
+        return false;
+    }
+
+    $importantPhones = new importantPhonesObject();
+    $importantPhones ->setTitle($data["title"]);
+    $importantPhones ->setContent($data["content"]);
+    $importantPhones ->setLink($data["link"]);
+
+    $this->_update($importantPhones);
+}
+
+    protected function _update(importantPhonesObject $importantPhones){
+
+    $query = "UPDATE ".$this->table." SET title='".$importantPhones->getTitle()."', content='".$importantPhones->getContent()."',link='".$importantPhones->getLink()."',
+        id='".$importantPhones->getID()."' WHERE id='".$importantPhones->getID()."'";
+
+    $res = $this->engine->dbase->insertQuery($query);
+
+    return $res;
+}
+
+    protected function _delete(importantPhonesObject $importantPhones){
+
+    $query = "DELETE ".$this->table." SET title='".$importantPhones->getTitle()."', content='".$importantPhones->getContent()."',link='".$importantPhones->getLink()."',
+        id='".$importantPhones->getID()."' WHERE id='".$importantPhones->getID()."'";
+
+    $res = $this->engine->dbase->insertQuery($query);
+
+    return $res;
+}
+}
