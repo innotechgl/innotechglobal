38a41070b582c6960bcae644833f72435d922dc2
Petar <milanoslav@yahoo.com>
2016-04-12 14:56:17 +0200

    Controller za important phones kreiran

--- utils/important_phones/controllers/importantPhonesController.php
+++ utils/important_phones/controllers/importantPhonesController.php
@@ -1,1 +1,25 @@
 <?php
+
+class importantPhonesController extends controller
+{
+
+    /** @var rest_class
+     */
+    protected $rest;
+
+    /**
+     * @param importantPhonesModel $model
+     */
+    public function __construct(importantPhonesModel $model)
+    {
+        importantPhones::__construct($model);
+        $this->model = $model;
+        $this->rest = new rest_class();
+        $this->resolve();
+    }
+
+    protected function add($data)
+    {
+        return $this->model->add($data);
+    }
+}
