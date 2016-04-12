765cb140f5a5027749b5f6f3afeb892c24958bb9
Blagoje <bstankovic95@gmail.com>
2016-04-12 18:46:48 +0200

    Changed to PHP files.
    Improved style.

---
+++ html/about_hotel_content.php
@@ -0,0 +1,29 @@
+<h1 class="text-center">Informations about hotel</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget efficitur nisi. In hac habitasse platea dictumst. 
+	Integer rhoncus molestie augue, quis elementum felis pretium eu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dictum 
+	vulputate malesuada. Cras non magna eu nisl euismod posuere sed vel nunc. Aliquam erat volutpat. Phasellus ultrices faucibus convallis. 
+	Vivamus hendrerit, urna ac facilisis vehicula, eros nunc fermentum massa, eleifend eleifend erat justo sed nibh.</p>
+
+<p>Suspendisse lobortis malesuada mauris quis ornare. Sed condimentum orci porta hendrerit laoreet. Aliquam in pretium d
+	olor. Donec ullamcorper accumsan condimentum. Cras non fringilla ligula, nec ultricies elit. Nullam facilisis vulputate elit eu cursus. 
+	Nam vestibulum laoreet arcu, eu rhoncus risus laoreet vitae. Curabitur porta, felis et ultrices porttitor, elit augue tincidunt massa, 
+	ac gravida dolor risus eget mauris. Donec in auctor erat, eu finibus lectus. Quisque auctor ipsum ante, tristique venenatis velit pelle
+	ntesque non. In accumsan sit amet ipsum sed rhoncus. Aliquam a blandit lacus. Quisque cursus sem sed purus bibendum cursus. Sed interdum
+	 faucibus lacus, ut maximus felis bibendum aliquet.</p>
+
+<p>Fusce et purus non magna fermentum ornare eu id turpis. Cras posuere fermentum condimentum. Nullam congue convallis
+ quam in fermentum. Proin ac tempus dolor. Praesent leo orci, varius vel felis vel, auctor vehicula ligula. Mauris sed rhoncus elit, fermen
+ tum lacinia elit. Nam non condimentum sem. Aliquam ac turpis vulputate, pulvinar urna eget, bibendum justo. Proin mattis, odio non eleifen
+ d pulvinar, mauris tellus feugiat arcu, vitae bibendum arcu metus in diam. Nam et nunc sagittis, elementum elit a, viverra sem. Ut in arcu 
+ quam.</p>
+
+
+<p>Ut consequat, lorem id sagittis convallis, eros neque pretium orci, ut sagittis sem dui a dui. In ultrices malesua
+	da odio, sed lacinia eros consectetur a. Maecenas in tellus id metus elementum porttitor. Fusce lacinia cursus fringilla. Fusce in lor
+	em est. Duis rutrum auctor sem a elementum. In vitae tortor erat. Ut tincidunt risus nec turpis scelerisque, ut tempor lacus scelerisque.
+</p>
+<div class="row margin-top">
+  <div class="col-md-4"><img src="hotel2.jpg" class="img-responsive img-border"></div>
+  <div class="col-md-4"><img src="hotel2.jpg" class="img-responsive img-border"></div>
+  <div class="col-md-4"><img src="hotel2.jpg" class="img-responsive img-border"></div>
+</div>
---
+++ html/index.php
@@ -0,0 +1,42 @@
+﻿<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP file</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UFT-8">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+ 
+  </head>
+<body>
+<?php
+include 'header.php';
+
+?>
+<hr class="hr">
+<div class="fluid-container"
+<div class="row">
+  <div class="col-md-3">
+      <?php
+            include 'baners.php';
+      ?></div>
+  <div class="col-md-6" id="justify"><?php
+
+  include('about_hotel_content.php');
+?>
+  </div>
+  <div class="col-md-3">
+      <?php
+            include 'menu.php';
+
+      ?>
+      <?php
+            include 'weather.php';
+
+      ?>
+</div>
+</div>
+</div>
+</body>
+</html>
