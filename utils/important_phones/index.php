b222ac47bd17cd3d6e9fa18b48bec3211181e66b
Petar <milanoslav@yahoo.com>
2016-04-12 14:49:27 +0200

    Index za important phones kreiran

--- utils/important_phones/index.php
+++ utils/important_phones/index.php
@@ -1,1 +1,9 @@
 <?php
+
+$this->engine->pages->load_page("important_phones","updater");
+
+require_once 'controllers/importantPhonesController.php';
+require_once 'models/importantPhonesModel.php';
+
+$model = new importantPhonesModel();
+$controller = new importantPhonesController($model);
