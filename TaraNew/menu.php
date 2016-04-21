4b6fdc637f18ac165673e827e761a0c2e88086cb
Petar <milanoslav@yahoo.com>
2016-04-21 19:00:44 +0200

    Update Cetvrtak, css, skripta js, meni i footer

--- TaraNew/menu.php
+++ TaraNew/menu.php
@@ -4,42 +4,42 @@
 <div id="wrapper nav nav-pills nav-stacked">
 
 	<img src="logohotela.png" class="logohotela">
-	<ul class="menu"> 
+	<ul class="menu" id="menu">
  
-		<li class="item1"><a href="#">DOGAĐAJI</a>
+		<li class="item1" id="oNama"><a href="#oNama">O NAMA</a>
+<!--			<ul>-->
+<!--				<li class="subitem1"><a href="#">Koncerti </a></li>-->
+<!--				<li class="subitem2"><a href="#">Dani hotela </a></li>-->
+<!--				<li class="subitem3"><a href="#">Konferencije </a></li>-->
+<!--			</ul>-->
+		</li>
+		<li class="item2" id="uOkolini"><a href="#uOkolini">U OKOLINI</a>
+<!--			<ul>-->
+<!--				<li class="subitem1"><a href="#">INFORMACIJE</a></li>-->
+<!--				<li class="subitem2"><a href="#">Dani hotela</a></li>-->
+<!--				<li class="subitem3"><a href="#">Dani hotela</a></li>-->
+<!--			</ul>-->
+		</li>
+		<li class="item3" id="dogadjaji"><a href="#dogadjaji">DOGAĐAJI</a>
 			<ul>
-				<li class="subitem1"><a href="#">Koncerti </a></li>
-				<li class="subitem2"><a href="#">Dani hotela </a></li>
-				<li class="subitem3"><a href="#">Konferencije </a></li>
+				<li class="subitem1" id="konferencije"><a href="#konferencije">Konferencije</a></li>
+				<li class="subitem2" id="ture"><a href="#ture">Ture</a></li>
+<!--				<li class="subitem3"><a href="#">Dani hotela</a></li>-->
 			</ul>
 		</li>
-		<li class="item2"><a href="#">U OKOLINI</a>
-			<ul>
-				<li class="subitem1"><a href="#">INFORMACIJE</a></li>
-				<li class="subitem2"><a href="#">Dani hotela</a></li>
-				<li class="subitem3"><a href="#">Dani hotela</a></li>
-			</ul>
+		<li class="item4" id="usluge"><a href="#usluge">USLUGE</a>
+<!--			<ul>-->
+<!--				<li class="subitem1"><a href="#">Cute Kittens</a></li>-->
+<!--				<li class="subitem2"><a href="#">Dani hotela</a></li>-->
+<!--				<li class="subitem3"><a href="#">Dani hotela</a></li>-->
+<!--			</ul>-->
 		</li>
-		<li class="item3"><a href="#">REZERVACIJE</a>
-			<ul>
-				<li class="subitem1"><a href="#">Cute Kittens</a></li>
-				<li class="subitem2"><a href="#">Dani hotela</a></li>
-				<li class="subitem3"><a href="#">Dani hotela</a></li>
-			</ul>
-		</li>
-		<li class="item4"><a href="#">O NAMA</a>
-			<ul>
-				<li class="subitem1"><a href="#">Cute Kittens</a></li>
-				<li class="subitem2"><a href="#">Dani hotela</a></li>
-				<li class="subitem3"><a href="#">Dani hotela</a></li>
-			</ul>
-		</li>
-		<li class="item5"><a href="#">DOGAĐAJI</a>
-			<ul>
-				<li class="subitem1"><a href="#">Cute Kittens</a></li>
-				<li class="subitem2"><a href="#">Konferencije</a></li>
-				<li class="subitem3"><a href="#">Konferencije</a></li>
-			</ul>
+		<li class="item5" id="rezervacije"><a href="#rezervacije">REZERVACIJE</a>
+<!--			<ul>-->
+<!--				<li class="subitem1"><a href="#">Cute Kittens</a></li>-->
+<!--				<li class="subitem2"><a href="#">Konferencije</a></li>-->
+<!--				<li class="subitem3"><a href="#">Konferencije</a></li>-->
+<!--			</ul>-->
 		</li>
 	</ul>
 
