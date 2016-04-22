e453511dd01d8785134a6dd0503adb24f345563f
Petar <milanoslav@yahoo.com>
2016-04-22 16:25:37 +0200

    Fixed weather widget to Belgrade, and css fixed!

--- TaraNew/index2.php
+++ TaraNew/index2.php
@@ -157,7 +157,7 @@
         <div class="weather-wrapper ">
             <img src="" class="weather-icon" alt="Weather Icon" />
 
-            <p><strong></strong>
+            <p><strong>Place</strong>
                 <br /><span class="weather-place"></span></p>
 
             <p><strong>Temperature</strong>
@@ -175,13 +175,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script type="text/javascript">
-    if (typeof jQuery == 'undefined') {
-        document.write(unescape("%3Cscript src='src/js/lib/jquery.1.9.1.min.js' type='text/javascript'%3E%3C/script%3E"));
-    }
-</script>
-<script src="js/plugins/src/openWeather.js"></script>
+
 
 <script type="text/javascript">
     $(function () {
@@ -216,13 +210,24 @@
 
 </script>
 
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript">
+    if (typeof jQuery == 'undefined') {
+        document.write(unescape("%3Cscript src='src/js/lib/jquery.1.9.1.min.js' type='text/javascript'%3E%3C/script%3E"));
+    }
+</script>
+<script src="js/plugins/src/openWeather.js"></script>
+
 <script>
 
     $(function() {
 
         $('.weather-temperature').openWeather({
             key: 'c9d49310f8023ee2617a7634de23c2aa',
-            city: 'Toronto, ON',
+//            city: 'Belgrade, RS',
+            lat: '44.804008',
+            lng: '20.46513',
+
             descriptionTarget: '.weather-description',
             windSpeedTarget: '.weather-wind-speed',
             minTemperatureTarget: '.weather-min-temperature',
