32debaefc11f873f515e9886d3e743a50f68c679
Petar <milanoslav@yahoo.com>
2016-04-22 12:30:18 +0200

    Weather widget made local

--- TaraNew/weather.php
+++ TaraNew/weather.php
@@ -2,4 +2,5 @@
 <!--
 By accessing and/or using this code snippet, you agree to AccuWeather’s terms and conditions (in English) which can be found at http://www.accuweather.com/en/free-weather-widgets/terms and AccuWeather’s Privacy Statement (in English) which can be found at http://www.accuweather.com/en/privacy.
 -->
-</a><div id="awcc1460461301867" class="aw-widget-current"  data-locationkey="298198" data-unit="c" data-language="en-us" data-useip="false" data-uid="awcc1460461301867"></div><script type="text/javascript" src="http://oap.accuweather.com/launch.js"></script>
+</a><div id="awcc1460461301867" class="aw-widget-current"  data-locationkey="298198" data-unit="c" data-language="en-us" data-useip="false" data-uid="awcc1460461301867"></div><script type="text/javascript" src="js/weather_accu.js"></script>
+<!--http://oap.accuweather.com/launch.js-->
