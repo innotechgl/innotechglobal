5c4bcf97897c79136fec51bdc7ee8df2c1bb9e90
Petar <milanoslav@yahoo.com>
2016-04-25 14:09:34 +0200

    Important phones created, and content

---
+++ TaraNew/important_phones_content.php
@@ -0,0 +1,19 @@
+<div class="row" >
+<div class="ip-main ">
+    <div class="ip-content">
+        <h2>VAŽNI TELEFONI</h2>
+        <p> HITNA POMOĆ: 193</p>
+        <p> POLICIJA: 194</p>
+        <p> VATROGASCI: 192</p>
+        <p> TAČNO VREME: 195</p>
+        <p> VATROGASCI: 192</p>
+        <p> VATROGASCI: 192</p>
+        <p> VATROGASCI: 192</p>
+        <p> VATROGASCI: 192</p>
+        <p> SERVIS: 19212323</p>
+        <p> SERVIS: 19212323</p>
+        <p> SERVIS: 19212323</p>
+        <p> SERVIS: 19212323</p>
+    </div>
+</div>
+</div>
