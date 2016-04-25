9efbd51a771ed2d5f426181d898083fb5a655df3
Petar <milanoslav@yahoo.com>
2016-04-25 16:25:14 +0200

    Events created and content!!!

--- TaraNew/events.php
+++ TaraNew/events.php
@@ -1,7 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP file</title>
+    <?php include "html_header.php" ?>
+<body>
+<div class="fluid-container">
+    <div class="col-md-9" id="containment">
+
+        <?php include "top_baner.php" ?>
+        <?php include "events_content.php" ?>
+        
+
+        <div class="col-md-9" id="footer">
+            <?php include "footer.php"?>
+        </div>
+    </div>
+    <!-- menu and weather -->
+
+    <div class="col-md-3" id="right-menu">
+        <?php
+
+        include "menu.php";
+        ?>
+        <?php include "weather2.php";
+        ?>
+        <div class="tara">
+            <img src="images/taralogo.png" class="img-responsive taralogo">
+        </div>
+    </div>
+
+</div>
+</div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<?php include "menu_script.php" ?>
 <?php
-/**
- * Created by PhpStorm.
- * User: KORISNIK
- * Date: 4/21/2016
- * Time: 8:16 PM
- */
+include "weather-script.php"
+?>
+</body>
+</head>
+</html>
---
+++ TaraNew/events_content.php
@@ -0,0 +1,40 @@
+<div class="row">
+    <div class="ip-main ">
+        <div class="row event-content">
+            <div class="top-content col-md-12">
+                <div class="event-black">
+                    1
+                </div>
+                <div class="event-grey">
+                    2
+                </div>
+                <div class="event-black">
+                    3
+                </div>
+                <div class="event-grey">
+                    4
+                </div>
+                <div class="event-black">
+                    5
+                </div>
+            </div>
+            <div class="bottom-content col-md-12">
+                <div class="event-grey">
+                    6
+                </div>
+                <div class="event-black">
+                    7
+                </div>
+                <div class="event-grey">
+                    8
+                </div>
+                <div class="event-black">
+                    9
+                </div>
+                <div class="event-grey">
+                    10
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
