a55e4030c135f50ff9c1d107d876024489dbc55e
Petar <milanoslav@yahoo.com>
2016-04-26 19:40:43 +0200

    Created event_single and content! Minor bugs and fixes

---
+++ TaraNew/event_single_content.php
@@ -0,0 +1,26 @@
+<div class="row">
+    <div class="ip-main ">
+        <div class="es-content">
+
+            <div class="es-heading">
+                <img class="wrap" src="images/1.jpg" >
+                <button class="es-button">NAZAD</button>
+                <h2>EVENT 1</h2>
+                <div class="button-only">
+                </div>
+            </div>
+            <hr class="es-linija">
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse non commodo nisl, vitae pulvinar
+                dui. Aliquam nec consequat elit, sit amet varius risus. Quisque ac mi convallis, suscipit mi sit amet,
+                iaculis velit. Sed ultrices felis turpis, nec posuere metus molestie eget. Nullam molestie hendrerit
+                massa. Etiam id pulvinar est. Nam vulputate molestie est, ac cursus diam aliquam at. Donec hendrerit
+                magna eros, et pretium dolor faucibus in. Donec venenatis neque a gravida facilisis. Nulla lectus neque,
+                porta vitae maximus imperdiet, faucibus id nunc. Donec malesuada tristique odio, at ullamcorper purus
+                blandit a. Donec at arcu imperdiet, euismod mi sed, pellentesque ante. Cum sociis natoque penatibus et
+                magnis dis parturient montes, nascetur ridiculus mus. Duis tincidunt elit eu auctor tincidunt. Cras
+                egestas nunc quis neque iaculis, ac porttitor odio varius. Aliquam erat volutpat. Etiam at eros vitae
+                dolor tincidunt bibendum. Donec laoreet ultrices neque ac mattis. </p>
+        </div>
+    </div>
+</div>
