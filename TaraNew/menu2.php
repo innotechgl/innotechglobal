1a20da50d6a80ba774aed02480ea56bbe06f3ea7
Blagoje <bstankovic95@gmail.com>
2016-05-04 15:20:07 +0200

    Meni shorted

--- TaraNew/menu2.php
+++ TaraNew/menu2.php
@@ -6,42 +6,44 @@
 
     <ul class="right-menu-ul" id="right-menu-ul">
 
-        <li class="item1" ><a href="#" id="id-color-right-menu">O HOTELU</a>
-            <ul class="sub-list">
-                <li ><a href="#" id="id-color-right-menu" data-target="1" class="nav-btn nav-page1">O nama </a></li>
-                <li ><a href="#" id="id-color-right-menu" data-target="3" class="nav-btn nav-page3">Važni telefoni</a></li>
-                <li ><a href="#" id="id-color-right-menu" data-target="4" class="nav-btn nav-page4">Plan hotela </a></li>
+        <li class="item1" ><a href="#">O HOTELU</a>
+            <ul>
+                <li ><a href="#" data-target="1" class="nav-btn nav-page1">O nama </a></li>
+                <li ><a href="#" data-target="3" class="nav-btn nav-page3">Važni telefoni</a></li>
+                <li ><a href="#" data-target="4" class="nav-btn nav-page4">Plan hotela </a></li>
             </ul>
         </li>
-        <li class="item2"><a href="#" id="id-color-right-menu">U OKOLINI</a>
-            <ul class="sub-list">
-                <li class="" ><a href="#" id="id-color-right-menu">INFORMACIJE</a></li>
-                <li class="" ><a href="#" id="id-color-right-menu">Dani hotela</a></li>
-                <li class="" ><a href="#" id="id-color-right-menu">Dani hotela</a></li>
-           </ul>
+        <li class="item2"><a href="#">U OKOLINI</a>
+           <!-- <ul>
+                <li class="" ><a href="#">INFORMACIJE</a></li>
+                <li class="" ><a href="#">Dani hotela</a></li>
+                <li class="" ><a href="#">Dani hotela</a></li>
+           </ul> -->
         </li>
 
-        <li class="item3" ><a href="#" id="id-color-right-menu">DOGAĐAJI</a>
-            <ul class="sub-list">
-                <li ><a href="#" id="id-color-right-menu" data-target="5" class="nav-btn nav-page5">Konferencije</a></li>
-                <li class="" id="ture"><a href="#" id="id-color-right-menu">Ture</a></li>
-                <li class="" ><a href="#" id="id-color-right-menu">Dani hotela</a></li>
+        <li class="item3" ><a href="#">DOGAĐAJI</a>
+            <ul>
+                <li ><a href="#" data-target="5" class="nav-btn nav-page5">Konferencije</a></li>
+                <li class="" id="ture"><a href="#">Ture</a></li>
+                <li class="" ><a href="#">Dani hotela</a></li>
             </ul>
         </li>
-        <li class="item4"><a href="#" id="id-color-right-menu">USLUGE</a>
-            <ul class="sub-list">
-                <li class="" ><a href="#" id="id-color-right-menu">Cute Kittens</a></li>
-                <li class="" ><a href="#" id="id-color-right-menu">Dani hotela</a></li>
-                <li class="" ><a href="#" id="id-color-right-menu">Dani hotela</a></li>
-            </ul>
+        <li class="item4"><a href="#">USLUGE</a>
+           <!-- <ul>
+                <li class="" ><a href="#">Cute Kittens</a></li>
+                <li class="" ><a href="#">Dani hotela</a></li>
+                <li class="" ><a href="#">Dani hotela</a></li>
+            </ul> -->
         </li>
-        <li class="item5" id="rezervacije"><a href="#" id="id-color-right-menu">REZERVACIJE</a>
-            <ul class="sub-list"f>
-                <li class="" ><a href="#" id="id-color-right-menu">Cute Kittens</a></li>
-                <li class="" ><a href="#" id="id-color-right-menu">Konferencije</a></li>
-                <li class="" ><a href="#" id="id-color-right-menu">Konferencije</a></li>
-            </ul>
+        <li class="item5" id="rezervacije"><a href="#">REZERVACIJE</a>
+            <!-- <ul>
+                <li class="" ><a href="#">Cute Kittens</a></li>
+                <li class="" ><a href="#">Konferencije</a></li>
+                <li class="" ><a href="#">Konferencije</a></li>
+            </ul> -->
         </li>
     </ul>
 
 </div>
+
+
