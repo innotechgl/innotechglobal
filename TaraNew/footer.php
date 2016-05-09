d680770c54a3fe416d8fe1b30202a1e66b394965
InnotechGlobal <dajana.nestorovic@innotechgl.com>
2016-05-09 15:19:43 +0200

    world clock fixed

--- TaraNew/footer.php
+++ TaraNew/footer.php
@@ -1,43 +1,44 @@
 <div class="col-md-2">
     <div class="time-text">
         BELGRADE
-        </div>
-        <div class="time">
-    <script src="http://www.clocklink.com/embed.js"></script> <script type="text/javascript" language="JavaScript">obj=new Object;obj.clockfile="5012-white.swf";obj.TimeZone="Serbia_Belgrade"; obj.width=91;obj.height=30;obj.wmode="transparent";showClock(obj);</script>
+    </div>
+    <div class="time">
+        <iframe scrolling="no" frameborder="no" clocktype="html5" style="overflow:hidden;border:0;margin:0;padding:0;width:120px;height:40px;"src="http://www.clocklink.com/html5embed.php?clock=004&timezone=Serbia_Belgrade&color=gray&size=120&Title=&Message=&Target=&From=2016,1,1,0,0,0&Color=#262626"></iframe>
 
     </div>
-    </div>
+</div>
 <div class="col-md-2 ">
     <div class="time-text">
         ANKARA
     </div>
     <div class="time">
-        <script src="http://www.clocklink.com/embed.js"></script> <script type="text/javascript" language="JavaScript">obj=new Object;obj.clockfile="5012-white.swf";obj.TimeZone="Turkey_Ankara"; obj.width=91;obj.height=30;obj.wmode="transparent";showClock(obj);</script></div></div>
+        <iframe scrolling="no" frameborder="no" clocktype="html5" style="overflow:hidden;border:0;margin:0;padding:0;width:120px;height:40px;"src="http://www.clocklink.com/html5embed.php?clock=004&timezone=Turkey_Ankara&color=gray&size=120&Title=&Message=&Target=&From=2016,1,1,0,0,0&Color=gray"></iframe>
+    </div></div>
 <div class="col-md-2">
     <div class="time-text">
         PARIS
     </div>
     <div class="time">
-        <script src="http://www.clocklink.com/embed.js"></script> <script type="text/javascript" language="JavaScript">obj=new Object;obj.clockfile="5012-white.swf";obj.TimeZone="France_Paris"; obj.width=91;obj.height=30;obj.wmode="transparent";showClock(obj);</script>
+        <iframe scrolling="no" frameborder="no" clocktype="html5" style="overflow:hidden;border:0;margin:0;padding:0;width:120px;height:40px;"src="http://www.clocklink.com/html5embed.php?clock=004&timezone=France_Paris&color=gray&size=120&Title=&Message=&Target=&From=2016,1,1,0,0,0&Color=gray"></iframe>
     </div></div>
 <div class="col-md-2 ">
     <div class="time-text">
         ATHENS
     </div>
     <div class="time">
-        <script src="http://www.clocklink.com/embed.js"></script> <script type="text/javascript" language="JavaScript">obj=new Object;obj.clockfile="5012-white.swf";obj.TimeZone="Greece_Athens"; obj.width=91;obj.height=30;obj.wmode="transparent";showClock(obj);</script>
+        <iframe scrolling="no" frameborder="no" clocktype="html5" style="overflow:hidden;border:0;margin:0;padding:0;width:120px;height:40px;"src="http://www.clocklink.com/html5embed.php?clock=004&timezone=Greece_Athens&color=gray&size=120&Title=&Message=&Target=&From=2016,1,1,0,0,0&Color=gray"></iframe>
     </div></div>
 <div class="col-md-2 ">
     <div class="time-text">
-        KYOTO
+        TOKYO
     </div>
     <div class="time">
-        <script src="http://www.clocklink.com/embed.js"></script> <script type="text/javascript" language="JavaScript">obj=new Object;obj.clockfile="5012-white.swf";obj.TimeZone="Japan_Kyoto"; obj.width=91;obj.height=30;obj.wmode="transparent";showClock(obj);</script>
+        <iframe scrolling="no" frameborder="no" clocktype="html5" style="overflow:hidden;border:0;margin:0;padding:0;width:120px;height:40px;"src="http://www.clocklink.com/html5embed.php?clock=004&timezone=Japan_Tokyo&color=gray&size=120&Title=&Message=&Target=&From=2016,1,1,0,0,0&Color=gray"></iframe>
     </div></div>
 <div class="col-md-2 ">
     <div class="time-text">
         MOSCOW
     </div>
     <div class="time">
-        <script src="http://www.clocklink.com/embed.js"></script> <script type="text/javascript" language="JavaScript">obj=new Object;obj.clockfile="5012-white.swf";obj.TimeZone="Russia_Moscow"; obj.width=91;obj.height=30;obj.wmode="transparent";showClock(obj);</script>
+        <iframe scrolling="no" frameborder="no" clocktype="html5" style="overflow:hidden;border:0;margin:0;padding:0;width:120px;height:40px;"src="http://www.clocklink.com/html5embed.php?clock=004&timezone=Russia_Moscow&color=gray&size=120&Title=&Message=&Target=&From=2016,1,1,0,0,0&Color=gray"></iframe>
     </div></div>
